2cc150352eff750e7943e5d2e5e80058f210ab89
Flochoon <florentin.lochon35@gmail.com>
2023-10-18 11:11:29 +0200

    URL routing

--- PyTaskManager.py
+++ PyTaskManager.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from markupsafe import escape
+from flask import url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='Flo'))
